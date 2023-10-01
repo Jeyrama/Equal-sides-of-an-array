cb18e8855d9ed8f807173b70d3310ed3bfa16fbc
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-10-01 12:11:26 -0700

    Update Equal-sides-of-an-array.js

--- Equal-sides-of-an-array.js
+++ Equal-sides-of-an-array.js
@@ -37,7 +37,19 @@ then you will return -1.
 
 Note:
 If you are given an array with multiple answers, return the lowest correct index.
-/*
+*/
 
 
 // Solution
+
+function findEvenIndex(arr) {
+  let left = 0, right = arr.reduce(function(pv, cv) { return pv + cv; }, 0);
+  for(let i = 0; i < arr.length; i++) {
+      if(i > 0) left += arr[i-1];
+      right -= arr[i];
+      
+      if(left == right) return i;
+  }
+  
+  return -1;
+}
